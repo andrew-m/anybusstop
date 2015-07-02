e75c135412cbcf8b71a5f30a80ed7f747346eef1
andrew-m <andrew.maddison@thoughtworks.com>
2015-07-02 09:06:22 +0100

    Got async load of help text to work, badly styled so far and text is rubbish.

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,16 @@
 $(function () {
+    console.log('in document ready');
     $('#target').submit(function(event){
+        console.log('in submit');
         var number = $('#number-textbox').val();
         event.preventDefault();
         window.location.href = "http://m.countdown.tfl.gov.uk/arrivals/" + number;
     });
+    console.log('tried set submit');
+    $('.help').click(function(event){
+        console.log('in help click');
+        $('.help-placeholder' ).load( "/help.html #container" );
+        //event.preventDefault();
+    });
+    console.log('tried set help click');
 });
