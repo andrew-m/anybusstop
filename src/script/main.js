1e97d71b8e1e79d5b9ce52d89c72ca770dc69cdd
andrew-m <andrew.maddison@thoughtworks.com>
2015-07-09 09:57:23 +0100

    Should work on enter key as well as button click.

--- src/script/main.js
+++ src/script/main.js
@@ -1,11 +1,17 @@
 $(function () {
     console.log('in document ready');
-    $('.submit-button').click(function(event){
+    var gofunc = function (event){
         console.log('in submit');
         var number = $('#number-textbox').val();
         event.preventDefault();
         window.location.href = "http://m.countdown.tfl.gov.uk/arrivals/" + number;
-    });
+    }
+    $('.submit-button').click(gofunc);
+    $('#number-textbox').on("keyup",(function(e) {
+            if(e.which == 13) {
+                $('.submit-button').click();
+            }
+        }));
     console.log('tried set submit');
     $('.help-button').click(function(event){
         console.log('in help click');
